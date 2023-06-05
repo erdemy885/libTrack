82a6d0948b5ef8002fabf0b83c00ee3fa37fe73c
Erdem Yilmaz <erdemyilmaz@Erdems-Air.fios-router.home>
2023-06-05 02:05:41 -0400

    basic signup system

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return self.username
+    
 
 @app.route('/dashboard')
 def dashboard():
@@ -10,7 +23,20 @@ def dashboard():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        return request.form['username'] + " " + request.form['password']
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            return redirect(url_for('signup'))
+        
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return str(new_user.id)
     if request.method == 'GET':
         return render_template('credentials.html', title="Sign up", current_url=request.url)
 
