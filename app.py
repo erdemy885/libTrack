c63ffc45f09562cfd5880b2d4a4a507b80acd5fb
Erdem Yilmaz <erdemyilmaz@Erdems-Air.fios-router.home>
2023-06-11 13:51:49 -0400

    refactor code to use absolute imports and set app.py as entry point

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from os import path
 
 db = SQLAlchemy()
 
-def create_app():
+if __name__ == "__main__":
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key'
@@ -13,23 +13,22 @@ def create_app():
 
     db.init_app(app)
 
-    from .models import User
+    from models import User
 
     with app.app_context():
         db.create_all()
 
-    login_manager = LoginManager()
+    login_manager = LoginManager(app)
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    from main import main
+    app.register_blueprint(main)
 
-    return app
+    from auth import auth
+    app.register_blueprint(auth)
+
+    app.run(debug=True)
