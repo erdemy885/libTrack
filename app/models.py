3a559e0bcbf64a6df86c316a70340a2dfb8ef02b
erdemy885 <erdemy885@gmail.com>
2023-06-12 19:09:00 -0400

    add functionality to create new categories

--- app/models.py
+++ app/models.py
@@ -5,3 +5,20 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+    categories = db.relationship('Category', backref='user')
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    num = db.Column(db.Integer)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(5), nullable=False)
+    shelf = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    books = db.relationship('Book', backref='category')
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    num_in_series = db.Column(db.Integer)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
+from ..models import Category
+from .. import db
 
 main = Blueprint('main', __name__)
 
@@ -21,3 +23,21 @@ def scanner():
 @login_required
 def dashboard():
     return render_template('dashboard.html', name=current_user.username)
+
+@main.route('/newcategory', methods=['GET', 'POST'])
+@login_required
+def create_category():
+    if request.method == 'POST':
+        num = Category.query.filter_by(user_id=current_user.id).count() + 1
+        name = request.form.get('name')
+        code = request.form.get('code')
+        shelf = request.form.get('shelf')
+
+        new_category = Category(num=num, name=name, code=code, shelf=shelf, user=current_user)
+
+        db.session.add(new_category)
+        db.session.commit()
+
+        return redirect(url_for('main.dashboard'))
+    if request.method == 'GET':
+        return render_template('create_category.html')
