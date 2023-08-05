f97e79f34aada9743c6f11f84f01d93025317d6a
erdemy885 <erdemy885@gmail.com>
2023-08-04 21:39:04 -0400

    add logo

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -3,9 +3,16 @@ const Welcome = () => {
     <>
       <div className="bg-orange-200">
         <div className="grid grid-rows-4 place-items-center h-screen">
+          <img className="row-start-1" src="logo.png"></img>
           <div className="flex flex-col items-center text-9xl font-radley row-start-2">
             <div>Welcome to</div>
             <div className="italic text-amber-600">libTrack</div>
+          </div>
+          <div className="text-5xl row-start-3">
+            Track and store your books online
+          </div>
+          <div className="text-3xl row-start-4 text-bold text-amber-600">
+            Login | Sign up
           </div>
         </div>
       </div>
