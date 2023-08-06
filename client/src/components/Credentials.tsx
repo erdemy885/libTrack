c8e80523dfb73a89f02df9fd28ed0dddf3042d67
Erdem Yilmaz <erdemyilmaz@Erdems-Air.fios-router.home>
2023-08-06 03:29:20 -0400

    finish making form

--- client/src/components/Credentials.tsx
+++ client/src/components/Credentials.tsx
@@ -1,18 +1,18 @@
 const Credentials = () => {
   return (
     <>
-      <div className="px-16 pt-11 bg-amber-700 rounded-3xl grid grid-rows-4 row-start-4 place-items-center space-y-5">
-        <div className="w-80">
+      <div className="w-1/2 h-96 bg-amber-700 rounded-3xl place-items-center grid grid-rows-4">
+        <div className="row-start-1 w-60">
           <div className="text-3xl">Username</div>
-          <input type="text" className="w-full"></input>
+          <input type="text" className="w-full h-10 rounded-md"></input>
         </div>
-        <div className="w-80">
+        <div className="row-start-2 w-60">
           <div className="text-3xl">Password</div>
-          <input type="text" className="w-full"></input>
+          <input type="text" className="w-full h-10 rounded-md"></input>
         </div>
-        <div className="w-80">
+        <div className="row-start-3 w-60">
           <div className="text-3xl">Confirm Password</div>
-          <input type="text" className="w-full"></input>
+          <input type="text" className="w-full h-10 rounded-md"></input>
         </div>
       </div>
     </>
--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -32,9 +32,27 @@ const Welcome = () => {
             <div className="text-4xl font-radley row-start-1">
               Returning user?
             </div>
+
             <div className="text-8xl font-radley row-start-2">Log in</div>
 
-            <Credentials />
+            <div className="w-1/2 px-12 py-8 space-y-5 bg-amber-700 rounded-3xl place-items-center row-start-4">
+              <div className="w-full">
+                <div className="text-3xl">Username</div>
+                <input type="text" className="w-full h-10 rounded-md"></input>
+              </div>
+              <div className="w-full">
+                <div className="text-3xl">Password</div>
+                <input type="text" className="w-full h-10 rounded-md"></input>
+              </div>
+              <div className="w-full">
+                <div className="text-3xl">Confirm Password</div>
+                <input type="text" className="w-full h-10 rounded-md"></input>
+              </div>
+              <div className="flex items-center w-full">
+                <button className="border border-black rounded-lg h-5 w-5 mr-2 hover:bg-amber-800"></button>
+                <div className="text-2xl">Remember me</div>
+              </div>
+            </div>
 
             <button className="bg-amber-700 rounded-full row-start-6 px-9 py-5 hover:bg-amber-800">
               <div className="font-radley text-6xl">Log in</div>
