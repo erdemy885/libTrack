b52da29ce40adf62231f397d460af2551b3661e9
erdemy885 <erdemy885@gmail.com>
2023-08-07 17:23:47 -0400

    fix proportions and finalize design for welcome page

--- client/src/pages/Welcome.tsx
+++ client/src/pages/Welcome.tsx
@@ -7,18 +7,16 @@ const Welcome = () => {
   return (
     <>
       <div className="bg-orange-200">
-        <div className="grid grid-rows-4 place-items-center h-screen">
-          <img className="row-start-1" src="logo.png"></img>
-          <div className="flex flex-col items-center text-9xl font-radley row-start-2">
-            <div>Welcome to</div>
-            <div className="italic text-amber-600">libTrack</div>
+        <div className="flex flex-col justify-around items-center h-screen">
+          <img src="logo.png"></img>
+          <div className="flex flex-col items-center text-9xl font-radley">
+            <span>Welcome to</span>
+            <span className="italic text-amber-600">libTrack</span>
           </div>
-          <div className="text-5xl row-start-3">
-            Track and store your books online
-          </div>
+          <span className="text-5xl">Track and store your books online</span>
           <a
             href=""
-            className="text-3xl row-start-4 text-bold text-amber-600"
+            className="text-3xl text-bold text-amber-600"
             onClick={(event) => {
               event.preventDefault();
               targetRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -29,13 +27,58 @@ const Welcome = () => {
         </div>
 
         {/* login/signup page */}
-        <div ref={targetRef} className="h-screen w-screen">
+        <div ref={targetRef} className="h-screen flex">
+          {/* login */}
           <div className="bg-amber-600 h-full w-1/2 flex flex-col items-center justify-around">
             <div className="text-4xl font-radley">Returning user?</div>
             <div className="text-8xl font-radley">Log in</div>
-            <div className="bg-amber-700 w-1/3 h-1/2 rounded-3xl"></div>
-            <button className="bg-amber-700 text-6xl font-radley rounded-full py-5 px-10 hover:bg-amber-800">
+            <div className="bg-amber-700 w-1/2 h-1/2 rounded-3xl flex flex-col justify-around px-10">
+              <div className="text-3xl flex flex-col">
+                <span>Username</span>
+                <input type="text" className="rounded-lg"></input>
+              </div>
+              <div className="text-3xl flex flex-col">
+                <span>Password</span>
+                <input type="password" className="rounded-lg"></input>
+              </div>
+              <div className="text-3xl flex flex-col">
+                <span>Confirm Password</span>
+                <input type="password" className="rounded-lg"></input>
+              </div>
+              <div className="text-2xl flex mx-2 space-x-3 align-center">
+                <input type="checkbox"></input>
+                <span>Remember me</span>
+              </div>
+            </div>
+            <button className="bg-amber-700 text-6xl font-radley rounded-full py-3 px-6 hover:bg-amber-800">
               Log in
+            </button>
+          </div>
+
+          {/* signup */}
+          <div className="bg-amber-700 h-full w-1/2 flex flex-col items-center justify-around">
+            <div className="text-4xl font-radley">New user?</div>
+            <div className="text-8xl font-radley">Sign up</div>
+            <div className="bg-amber-600 w-1/2 h-1/2 rounded-3xl flex flex-col justify-around px-10">
+              <div className="text-3xl flex flex-col">
+                <span>Username</span>
+                <input type="text" className="rounded-lg"></input>
+              </div>
+              <div className="text-3xl flex flex-col">
+                <span>Password</span>
+                <input type="password" className="rounded-lg"></input>
+              </div>
+              <div className="text-3xl flex flex-col">
+                <span>Confirm Password</span>
+                <input type="password" className="rounded-lg"></input>
+              </div>
+              <div className="text-2xl flex mx-3 space-x-3 align-center">
+                <input type="checkbox"></input>
+                <span>Remember me</span>
+              </div>
+            </div>
+            <button className="bg-amber-600 text-6xl font-radley rounded-full py-3 px-6 hover:bg-amber-500">
+              Sign up
             </button>
           </div>
         </div>
